2bc2d661d47426fe87987734f6b0a864e4669136
Lukas <lukasurin@gmail.com>
2013-10-11 20:53:37 +0200

    Grafy2
    - Zmenena funkcia loadFiles. Pridany parameter isAdded ktora urcuje ci
    pridavame cisla z fileCount k menu suboru. Ked nie tak nacitavame jeden
    subor.
    - Zmeneny skript PlotSkript, do ktoreho som pridal ziskanie dalsich dat
    zo suboru sgaLog.fitness_stats, podla ktorych vygenerujeme dalsi
    samostatny graf. Taktie je v skripte zabudovane ukladanie vytvorenych
    grafov

--- EVACV1/grafy/PlotScript.m
+++ EVACV1/grafy/PlotScript.m
@@ -1,14 +1,22 @@
-defaultDirectory='d:\cvicenia\eva\EVACV1\sga\'
-fileName='sgaLog.fitness.'
+defaultDirectory='d:\cvicenia\eva\EVACV1\sga\';
+fileNameCounted='sgaLog.fitness.';
+fileNameFitness='sgaLog.fitness_stats';
 
 populationSize=50;
 colIndex1=1;
 colIndex2=2;
 vectorAdded=0:1:19;
 
-[X,Y]=loadFiles(strcat(defaultDirectory,fileName),vectorAdded,' ',colIndex1,colIndex2,populationSize);
+[X,Y]=loadFiles(strcat(defaultDirectory,fileNameCounted),vectorAdded,true,' ',colIndex1,colIndex2,populationSize);
+[X1,Y1]=loadFiles(strcat(defaultDirectory,fileNameFitness),1,false,' ',1,4,50);
 
 hold;
 plot(1:1:size(Y),Y(:,1),'r');
 plot(1:1:size(Y),Y(:,10),'b');
 plot(1:1:size(Y),Y(:,20),'g');
+saveas(gcf,'GenFitness','jpg')
+hold;
+
+figure
+plot(X1,Y1);
+saveas(gcf,'AllGenFitness','jpg');
--- EVACV1/grafy/loadFiles.m
+++ EVACV1/grafy/loadFiles.m
@@ -1,10 +1,18 @@
-function [ X, Y ] = loadFiles( file, vectorCount, delimiter, colindex1, colindex2,maxSize)
-    X=zeros(maxSize,size(vectorCount));
-    Y=zeros(maxSize,size(vectorCount));
-    for I = vectorCount
-        data=importdata(strcat(file,int2str(I)),delimiter);        
-        X(:,I+1)=data(:,colindex1);
-        Y(:,I+1)=data(:,colindex2);
+function [ X, Y ] = loadFiles( file, fileCount, isAdded, delimiter, colindex1, colindex2, maxSize)
+    X=zeros(maxSize,size(fileCount));
+    Y=zeros(maxSize,size(fileCount));
+    if isAdded 
+        for I = fileCount
+            data=importdata(strcat(file,int2str(I)),delimiter);        
+            X(:,I+1)=data(:,colindex1);
+            Y(:,I+1)=data(:,colindex2);
+        end
+    else
+        for I = fileCount
+            data=importdata(file,delimiter);        
+            X(:,I+1)=data(:,colindex1);
+            Y(:,I+1)=data(:,colindex2);
+        end        
     end
 end
 
